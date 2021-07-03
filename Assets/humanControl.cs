8dfc39ec616ce080e58f8e33cdcecf2b76e42652
DESKTOP-VO97ORH\Keisu <ekawano114@gmail.com>
2021-07-03 11:49:06 +0900

    fix ランダムに服装を生成

--- Assets/humanControl.cs
+++ Assets/humanControl.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 public class humanControl : MonoBehaviour
 {
     [SerializeField] private GameObject[] cloth;
+    [SerializeField] private GameObject[] beard;
+    [SerializeField] private GameObject[] props;
+    [SerializeField] private GameObject[] hair;
     
     // Start is called before the first frame update
     void Awake()
     {
-        int i = Random.Range(0, cloth.Length); // This creates a random number between 0 and the total number of enemies in the array (Make sure you have added enemies to the array in the inspector)
-        cloth[i].SetActive(true); // Creates that random enemy
+        int a = Random.Range(0, cloth.Length);
+        int c = Random.Range(0, beard.Length);
+        int e = Random.Range(0, hair.Length);
+        int g = Random.Range(0, props.Length);
+
+        
+        for (int i = 0; i < a; i++)
+        {
+            int b = Random.Range(0, cloth.Length); // This creates a random number between 0 and the total number of enemies in the array (Make sure you have added enemies to the array in the inspector)
+            cloth[b].SetActive(true);
+        }
+        beard[c].SetActive(true);
+        hair[e].SetActive(true);
+        props[g].SetActive(true);
+        
     }
 }
